11f72bff79460c23fc4a236bcdc3090820e64f11
Kristi <krsy3ii@yahoo.com>
2018-06-11 09:48:24 -0700

    add router

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,6 +1,6 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-
+import { FormsModule } from '@angular/forms'
 
 import { AppComponent } from './app.component';
 import { WelcomeComponent } from './welcome/welcome.component';
@@ -18,7 +18,10 @@ import { AlbumDetailComponent } from './album-detail/album-detail.component';
     AlbumDetailComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    FormsModule,
+    HttpModule,
+    routing
   ],
   providers: [],
   bootstrap: [AppComponent]
